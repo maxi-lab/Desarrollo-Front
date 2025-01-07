4dad0535b1c9fefd7bb01c520fb7d3f5dfa2a01b
Maximo <maxipreneste@gmail.com>
2025-01-07 20:58:03 -0300

    LocalStorage logIn

--- src/Context/UserContext.jsx
+++ src/Context/UserContext.jsx
@@ -1,10 +1,14 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState({rol:'',name:'',email:''});//esto tendria que estar en el local storage
-
+  const [user, setUser] = useState(()=>{
+    return JSON.parse(localStorage.getItem("User"))||{rol:'',name:'',email:''}
+  });//esto tendria que estar en el local storage
+  useEffect(()=>{
+    localStorage.setItem("User",JSON.stringify(user))
+  },[user])
   return (
     <UserContext.Provider value={{ user, setUser }}>
       {children}
