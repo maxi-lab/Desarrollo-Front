f976539613934a426b18b2417cea4a43f6eb2ff4
Maximo <maxipreneste@gmail.com>
2025-01-08 01:07:59 -0300

    las secciones expiran

--- src/Context/UserContext.jsx
+++ src/Context/UserContext.jsx
@@ -3,11 +3,24 @@ import React, { createContext, useEffect, useState } from 'react';
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
+
   const [user, setUser] = useState(()=>{
+    console.log('guardando en localstorage...')
     return JSON.parse(localStorage.getItem("User"))||{rol:'',name:'',email:''}
   });//esto tendria que estar en el local storage
   useEffect(()=>{
     localStorage.setItem("User",JSON.stringify(user))
+    if (user.rol||user.name||user.email) {
+      const timeout=setTimeout(()=>{
+        localStorage.clear()
+        alert("Sesion expirada")
+        
+        setUser({rol:'',name:'',email:''})
+       
+      },900000)
+      return ()=>clearTimeout(timeout)  
+    }
+    
   },[user])
   return (
     <UserContext.Provider value={{ user, setUser }}>
