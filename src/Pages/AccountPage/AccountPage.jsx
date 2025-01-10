0a3678ac69403aa3249765b1cbde7c4a8df6f69b
Maximo <maxipreneste@gmail.com>
2025-01-10 00:14:44 -0300

    consumo datos del back

--- src/Pages/AccountPage/AccountPage.jsx
+++ src/Pages/AccountPage/AccountPage.jsx
@@ -1,8 +1,36 @@
+import { useContext } from "react";
 import { Heading } from "../../Components/Heading/Headiing";
+import { UserContext } from "../../Context/UserContext";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import { Button, CircularProgress } from "@mui/material";
 
 export default function AccountPage(){
+    const {user}=useContext(UserContext)
+    const style = {
+        py: 0,
+        width: '100%',
+        maxWidth: 360,
+        borderRadius: 2,
+        border: '1px solid',
+        borderColor: 'divider',
+        backgroundColor: 'background.paper',
+      };
     return <>
         <Heading/>
         <h1>Cuenta</h1>
+        <List sx={style}>
+      <ListItem>
+        <ListItemText primary={`Nombre de usuario: ${user.userName}`} />
+      </ListItem>
+      <Divider component="li" />
+      <ListItem>
+        <ListItemText primary={`Correo electronico: ${user.email}`} />
+      </ListItem>
+      
+        </List>
+        <Button>Restablecer contraseña</Button>
     </>
 }
--- src/Pages/ProfilePage/ProfilePage.jsx
+++ src/Pages/ProfilePage/ProfilePage.jsx
@@ -1,10 +1,71 @@
+import { useContext, useEffect, useState } from "react";
 import { Heading } from "../../Components/Heading/Headiing";
+import { UserContext } from "../../Context/UserContext";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import { CircularProgress } from "@mui/material";
 
 export default function ProfilePage (){
-
+    const [profile,setProfile]=useState(null)
+    const {user}=useContext(UserContext)
+    const [isLoading,setIsLoading]=useState(true)
+    const [error,setError]=useState(null)
+    useEffect(()=>{
+        const requestOptions = {
+            method: "GET",
+            redirect: "follow"
+          };
+          
+          fetch(`https://localhost:7268/api/User/GetProfile?userName=${user.userName}`, requestOptions)
+            .then((response) => response.json())
+            .then((result) => {setProfile(result)
+                setIsLoading(false)
+            })
+            .catch((error) => {console.error(error)
+                setIsLoading(false)
+                setError(error)
+            });
+    },[])
+    const style = {
+        py: 0,
+        width: '100%',
+        maxWidth: 360,
+        borderRadius: 2,
+        border: '1px solid',
+        borderColor: 'divider',
+        backgroundColor: 'background.paper',
+      };
+      if (isLoading){
+        return <div style={{display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100vh',}}><CircularProgress />
+              </div>
+      }
+      if(setError!=null){
+        return<><h1>error</h1></>
+      }
+      //tengo que corregir la API
     return<>
         <Heading/>
-        <h1>Tu Perfil</h1>
+        <div>
+            <h1>Tu Perfil</h1>
+    <List sx={style}>
+      <ListItem>
+        <ListItemText primary={`Nombre: ${profile.nombre}`} />
+      </ListItem>
+      <Divider component="li" />
+      <ListItem>
+        <ListItemText primary={`Apellido: ${profile.apellido}`} />
+      </ListItem>
+      <Divider variant="inset" component="li" />
+      <ListItem>
+        <ListItemText primary={profile.rol==="Rescatista"?`Legajo: ${profile.nroIdentificador}`:`DNI: ${profile.nroIdentificador}`} />
+      </ListItem>
+        </List>
+        </div>
     
     </>
 }
