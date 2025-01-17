453aadaf3a05ff791a5c637eeda06e6ad6cd0dd0
Maximo <maxipreneste@gmail.com>
2025-01-17 16:37:15 -0300

    fix de la API del clima

--- src/data/WeatherAPI/WeatherAPI.jsx
+++ src/data/WeatherAPI/WeatherAPI.jsx
@@ -1,4 +1,4 @@
-export function recuperar() {
+export async function recuperar() {
 
    const response= fetch('https://api.weatherunlocked.com/api/resortforecast/54884003?app_id=30129f08&app_key=d9734555a33fbf13252c9234f2619bef')
     .then((response) => response.json())
